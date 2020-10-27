1ccc19bfa08d6cfa2ce84b3c1d88292af614cd1b
nrymarz <nrymarz@gmail.com>
2020-10-27 11:55:52 -0700

    commit tree e70bb40819962c98b2df77bce948a4ebc6a4983f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+    if array.length == 2
+        array.join(' and ')
+    elsif array.length > 2
+        array[array.length-1] = "and ".concat(array.last)
+        array.join(', ')
+    else
+        array.join
+    end
 
 end
